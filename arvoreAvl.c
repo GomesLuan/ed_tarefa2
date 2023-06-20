c4e9fc2c03b6f3979accaaed186c0cc110df61e7
Luan Victor <luan.araujo.gom@gmail.com>
2023-06-20 00:39:17 -0300

    Árvore AVL

--- arvoreAvl.c
+++ arvoreAvl.c
@@ -34,29 +34,107 @@
 
 struct tree_node
 {
+    struct tree_node *parent;
     int value;
+    unsigned int height;
     struct tree_node *lchild;
     struct tree_node *rchild;
 };
+
+void rd(struct tree_node *node) {
+    struct tree_node m = *node;
+    node->parent = node->lchild;
+    node->lchild = node->lchild->rchild;
+    m.lchild->parent = m.parent;
+    m.lchild->rchild = node;
+}
+
+void re(struct tree_node *node) {
+    struct tree_node m = *node;
+    node->parent = node->rchild;
+    node->rchild = node->rchild->lchild;
+    m.rchild->parent = m.parent;
+    m.rchild->lchild = node;
+}
+
+int cdiff(struct tree_node *node) {
+    int lheight, rheight;
+    if (node->lchild == NULL) {
+        lheight = 0;
+    }
+    else {
+        lheight = node->lchild->height;
+    }
+    if (node->rchild == NULL) {
+        rheight = 0;
+    }
+    else {
+        rheight = node->rchild->height;
+    }
+    return rheight - lheight;
+}
+
+int balance_case(struct tree_node *node) {
+    if (cdiff(node) < -1) {
+        if (cdiff(node->lchild) <= 0) {
+            return 1;
+        }
+        else {
+            return 3;
+        }
+    }
+    if (cdiff(node->rchild) >= 0) {
+        return 2;
+    }
+    return 4;
+}
 
 struct tree_node *tree_new(int v) {
     struct tree_node *w;
 
     w = (struct tree_node*) malloc(sizeof(struct tree_node));
+    w->parent = NULL;
     w->value = v;
+    w->height = 1;
     w->lchild = NULL;
     w->rchild = NULL;
 
     return w;
 }
 
+void balance(struct tree_node *node) {
+    node = node->parent;
+    while (node != NULL) {
+        if (cdiff(node) > 1 || cdiff(node) < -1) {
+            int c = balance_case(node);
+            if (c == 1) {
+                rd(node);
+            }
+            else if (c == 2) {
+                re(node);
+            }
+            else if (c == 3) {
+                re(node->lchild);
+                rd(node);
+            }
+            else if (c == 4) {
+                rd(node->rchild);
+                re(node);
+            }
+        }
+        node = node->parent;
+    }
+}
+
 void insert(struct tree_node **r, struct tree_node *w) {
     if ((*r) == NULL)
     {
         (*r) = w;
+        balance(w);
     }
     else
     {
+        w->parent = *r;
         if ((*r)->value < w->value)
             insert(&((*r)->rchild), w);
 	    else
@@ -75,6 +153,14 @@ struct tree_node *search(struct tree_node *r, int v) {
         return search(r->lchild, v);
     }
     return NULL;
+}
+
+void print(struct tree_node *r) {
+    if (r != NULL) {
+        print(r->lchild);
+        printf("%d\n", r->value);
+        print(r->rchild);
+    }
 }
 
 int main(int argc, char **argv) {
